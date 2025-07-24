b8a95c855ef8ff6609d00be74a9763fb46ece168
5581 <developer.1@maxvaluecredits.com>
2025-07-24 16:47:31 +0530

    Flat List

--- App.js
+++ App.js
@@ -67,7 +67,8 @@ export default function App() {
   );
 }*/
 
-//In this example,  we will create a responsive Button which displays Alert Component, using the react-native Button component.
+//** In this example,  we will create a responsive Button which displays Alert Component, using the react-native Button component.
+/*
 import * as React from "react";
 
 // Importing components from react-native library.
@@ -100,4 +101,75 @@ export default function App() {
 
     </View>
   );
+}*/
+
+// Flat List
+
+import React, { Component } from "react";
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import { Icon } from "react-native-elements";
+
+class App extends Component {
+  state = {
+    data: [
+      "Data Structures",
+      "STL",
+      "C++",
+      "Java",
+      "Python",
+      "ReactJS",
+      "Angular",
+      "NodeJs",
+      "PHP",
+      "MongoDb",
+      "MySql",
+      "Android",
+      "iOS",
+      "Hadoop",
+      "Ajax",
+      "Ruby",
+      "Rails",
+      ".Net",
+      "Perl",
+    ],
+  };
+
+  renderItem = ({ item }) => (
+    <View style={styles.row}>
+      <Text style={styles.rowText}>{item}</Text>
+      <Icon name="eye" type="ionicon" color="#C2185B" />
+    </View>
+  );
+
+  keyExtractor = (item, index) => index.toString();
+
+  render() {
+    return (
+      <View style={styles.screen}>
+        <FlatList
+          data={this.state.data}
+          renderItem={this.renderItem}
+          keyExtractor={this.keyExtractor}
+        />
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    marginTop: 30,
+  },
+  row: {
+    margin: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 2,
+  },
+  rowText: {
+    fontSize: 18,
+  },
+});
+
+export default App;
